2de35fb3cc57a2129a70e8bc8ce3ebb60c5698f8
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-07-27 12:36:38 -0600

    testing pull request

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
+const userList = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "jd@gmail.com",
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -14,6 +23,27 @@ router.post("/post-message", async (req, res) => {
     res.json({ serverMessage: response }).status(200);
   } catch (error) {
     res.json({ success: false }).status(500);
+  }
+});
+
+router.post("/create-user", async (req, res, next) => {
+  try {
+    const lastUser = userList[userList.length - 1];
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const newUser = {
+      id: Number(lastUser.id + 1),
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+    userList.push(newUser);
+
+    res.status(200).json({ message: "User has been added", success: true });
+  } catch (e) {
+    res.status(500).json({ message: "Error adding user." + e, success: false });
   }
 });
 module.exports = router;
