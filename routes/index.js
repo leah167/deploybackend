60919600602a3bef15afbb090b126f673a451209
Hansel Reynoso <omarreynoso09@gmail.com>
2022-07-27 11:44:16 -0400

    day 2 commit

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,47 @@
 var express = require("express");
 var router = express.Router();
+const { uuid } = require("uuidv4");
+
+const userList = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "jd@gmail.com",
+  },
+];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/post-message", async (req, res) => {
-  try {
-    const dateTime = new Date();
-    const clientMessage = req.body.clientMessage;
-    const response = `Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`;
-    res.json({ serverMessage: response }).status(200);
-  } catch (error) {
-    res.json({ success: false }).status(500);
-  }
+router.get("/get-users", function (req, res, next) {
+  res.json(userList);
 });
+
+router.post("/post-message", function (req, res, next) {
+  const clientMessage = req.body.clientMessage;
+  const dateTime = new Date();
+  res.json(
+    `Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`
+  );
+});
+
+router.post("/create-user", function (req, res, next) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const id = uuid();
+
+  const newUser = {
+    id,
+    firstName,
+    lastName,
+    email,
+  };
+  userList.push(newUser);
+  res.status(200).json(userList);
+});
+
 module.exports = router;
